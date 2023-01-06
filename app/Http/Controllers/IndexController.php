9e40f0bedadee574a6bbe56e677949581e07b4dd
Christian S <79862916+vchris5@users.noreply.github.com>
2023-01-06 15:58:19 -0500

    vvv

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return inertia(
+            'Index/Index',
+            [
+                'message' => 'Hello from Laravel'
+            ]
+        );
+    } // End index() method
+
+    public function show()
+    {
+        return inertia('Index/Show');
+    } // End show() method
+
+}
---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <MainLayout>
+        <div>Index</div>
+        <Link href="/hello">Show Page</Link>
+        <div>
+            This message is {{ message }}
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+import MainLayout from '../../Layouts/MainLayout.vue'
+defineProps({
+    message: String
+})
+</script>
