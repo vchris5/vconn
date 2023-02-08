db6d347bf1ee3bc8aae92605f26862eab9a59fc7
Christian S <79862916+vchris5@users.noreply.github.com>
2023-02-08 16:15:22 -0500

    2-8-0101110

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -9,7 +9,7 @@
           <Link :href="route('listing.index')">Connect</Link>
         </div>
         <div v-if="user" class="flex items-center gap-4">
-          <div class="text-sm text-gray-600 dark:text-gray-300">{{ user.name }}</div>
+          <Link :href="route('realtor.listing.index')" class="text-sm text-gray-600 dark:text-gray-300">{{ user.name }}</Link>
           <Link :href="route('listing.create')" class="btn-primary">+ New Listing</Link>
           <div>
             <Link :href="route('logout')" method="delete" as="button">Logout</Link>
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\UserAccountController;
+use App\Http\Controllers\RealtorListingController;
 use Illuminate\Support\Facades\Route;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -31,3 +31,10 @@ Route::post('login', [AuthController::class, 'store'])->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
 
 Route::resource('user-account', UserAccountController::class)->only(['create', 'store']);
+
+Route::prefix('realtor')
+    ->name('realtor.')
+    ->middleware('auth')
+    ->group(function () {
+        Route::resource('listing', RealtorListingController::class);
+    });
