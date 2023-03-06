7f5b2bacfb5143e72844597865086d0170bb5354
Christian S <79862916+vchris5@users.noreply.github.com>
2023-03-06 16:51:48 -0500

    3-6-0100101

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\UserAccountController;
 use App\Http\Controllers\RealtorListingController;
+use App\Http\Controllers\RealtorListingImageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -54,4 +55,7 @@ Route::prefix('realtor')
         Route::resource('listing', RealtorListingController::class)
             ->only(['index', 'destroy', 'edit', 'update', 'create', 'store'])
             ->withTrashed();
+
+        Route::resource('listing.image', RealtorListingImageController::class)
+            ->only(['create', 'store']);
     });
