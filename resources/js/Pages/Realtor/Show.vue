a1572fd93756430c1c9ef40a8803d24f7a50da7a
Christian S <79862916+vchris5@users.noreply.github.com>
2023-03-25 13:53:28 -0400

    3-25-0111011001

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -13,7 +13,13 @@
     </Box>
 
     <div v-else class="md:col-span-7 items-center">
-      displays when there is a offer!
+      <Offer
+        v-for="offer in listing.offers"
+        :key="offer.id"
+        class="mb-4"
+        :offer="offer"
+        :listing-price="listing.price"
+      />
     </div>
 
     <Box class="md:col-span-5">
@@ -33,6 +39,7 @@ import Price from '@/Components/Price.vue'
 import Box from '@/Components/UI/Box.vue'
 import { Link } from '@inertiajs/inertia-vue3'
 import { computed } from 'vue'
+import Offer from '@/Pages/Realtor/Show/Components/Offer.vue'
 
 const props = defineProps({ listing: Object })
 
