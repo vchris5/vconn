ae166b351368ae331210a2305abef30459898b9d
Christian S <79862916+vchris5@users.noreply.github.com>
2023-04-07 18:17:00 -0400

    notification count 4-7-00101101

---
+++ resources/js/Components/UI/EmptyState.vue
@@ -0,0 +1,14 @@
+
+import Box from './Box.vue';
+
+<template>
+  <Box>
+    <div class="w-full text-center font-medium text-gray-500">
+      <slot />
+    </div>
+  </Box>
+</template>
+
+<script setup>
+import Box from './Box.vue'
+</script>
