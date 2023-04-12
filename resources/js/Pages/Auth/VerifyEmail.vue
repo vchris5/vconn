d884989de3aa6595f2bea7d9ee94dd8ec632e664
Christian S <79862916+vchris5@users.noreply.github.com>
2023-04-12 15:21:49 -0400

    notification-email 4-12-00110111

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -1,3 +1,18 @@
 <template>
-  You must be verified to see this page!
+  <div>You must be verified to see this page!</div>
+  <div>
+    <Link
+      :href="route('verification.send')"
+      method="post"
+      as="button"
+      class="text-sky-600 dark:text-sky-400"
+    >
+      Resend verification email
+    </Link>
+  </div>
 </template>
+
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\OfferController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\UserAccountController;
-use App\Http\Controllers\OfferController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\RealtorListingController;
 use App\Http\Controllers\NotificationSeenController;
@@ -57,25 +58,32 @@ Route::put(
 )->middleware('auth')->name('notification.seen');
 
 Route::get('login', [AuthController::class, 'create'])
-    ->name('login');
+  ->name('login');
 Route::post('login', [AuthController::class, 'store'])
-    ->name('login.store');
+  ->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])
-    ->name('logout');
+  ->name('logout');
 
 Route::get('/email/verify', function () {
-    return inertia('Auth/VerifyEmail');
-})->middleware('auth')->name('verification.notice');
+  return inertia('Auth/VerifyEmail');
+})
+  ->middleware('auth')->name('verification.notice');
 
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
+  $request->fulfill();
 
-    return redirect()->route('listing.index')
-        ->with('success', 'Email was verified.');
+  return redirect()->route('listing.index')
+    ->with('success', 'Email was verified.');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
+Route::post('/email/verification-notification', function (Request $request) {
+  $request->user()->sendEmailVerificationNotification();
+
+  return back()->with('success', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
 Route::resource('user-account', UserAccountController::class)
-    ->only(['create', 'store']);
+  ->only(['create', 'store']);
 
 Route::prefix('realtor')
     ->name('realtor.')
